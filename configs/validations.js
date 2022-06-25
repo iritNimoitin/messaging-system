66fa6ff66e4bc4fd8757b5d86e8b06b8c6c1f521
Irit Nimoitin <irit@medi-software.com>
2022-06-25 17:42:34 +0300

    error handling

--- configs/validations.js
+++ configs/validations.js
@@ -5,7 +5,7 @@ module.exports = {
     return (req, res, next) => {
       const result = schema.validate(req.body);
       if (result.error) {
-        return result.error.details[0].message;
+        throw result.error.details[0].message;
       }
 
       if (!req.value) {
